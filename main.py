0e3722aa6eccbbaafd33f37e2e95279a73ef14c5
KateRastorgueva <rast6078@gmail.com>
2026-04-25 17:32:44 +0700

    cosmetics classification API

---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from PIL import Image
+import io
+import torch
+import torch.nn as nn
+from torchvision import transforms, models
+
+app = FastAPI(title="Cosmetics Classification API")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class_names = ['eyeshadow', 'foundation', 'lipstick', 'mascara', 'other']
+model = models.resnet18(weights=None)
+model.fc = nn.Linear(512, 5)
+model.load_state_dict(torch.load("model/cosmetics_model.pt", map_location=device))
+model = model.to(device)
+model.eval()
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# GET /health — проверка работоспособности сервиса (возвращает статус "ok");
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+#POST /predic
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    #валидацию входных данных (проверка формата и размера файла);
+    if file.content_type not in ["image/jpeg", "image/jpg", "image/png"]:
+        raise HTTPException(status_code=400, detail="Неверный формат")
+    contents = await file.read()
+    if len(contents) > 10 * 1024 * 1024:
+        raise HTTPException(status_code=400, detail="Файл больше 10 МБ")
+    
+    try:
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
+        image_tensor = transform(image).unsqueeze(0).to(device)
+        
+        with torch.no_grad():
+            output = model(image_tensor)
+            probabilities = torch.softmax(output, dim=1)
+            predicted_idx = torch.argmax(probabilities, dim=1).item()
+            predicted_class = class_names[predicted_idx]
+            probability = float(probabilities[0][predicted_idx])
+            all_classes = {class_names[i]: float(probabilities[0][i]) for i in range(len(class_names))}
+        
+        # возвращает JSON
+        return {
+            "class": predicted_class,
+            "probability": probability,
+            "all_classes": all_classes
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка {str(e)}")
+
+# реализуйте запуск приложения через Uvicorn.
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
