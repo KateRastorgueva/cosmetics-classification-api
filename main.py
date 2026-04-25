958e16ba16facdd09e639943d42d47a50165d205
KateRastorgueva <rast6078@gmail.com>
2026-04-25 19:03:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,17 +20,24 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# GET /health — проверка работоспособности сервиса (возвращает статус "ok");
+def get_prediction(image_tensor):
+    model.eval()
+    with torch.no_grad():
+        output = model(image_tensor)
+        probabilities = torch.softmax(output, dim=1)
+        predicted_class = torch.argmax(probabilities, dim=1).item()
+        return predicted_class, probabilities.cpu().numpy()[0]
+# GET /health — проверка работоспособности сервиса
 @app.get("/health")
 async def health():
     return {"status": "ok"}
 
-#POST /predic
+# POST /predict — принимает изображение, возвращает JSON
+# Валидация входных данных и обработка ошибок
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    #валидацию входных данных (проверка формата и размера файла);
     if file.content_type not in ["image/jpeg", "image/jpg", "image/png"]:
-        raise HTTPException(status_code=400, detail="Неверный формат")
+        raise HTTPException(status_code=400, detail="фНеверный формат")
     contents = await file.read()
     if len(contents) > 10 * 1024 * 1024:
         raise HTTPException(status_code=400, detail="Файл больше 10 МБ")
@@ -39,15 +46,12 @@ async def predict(file: UploadFile = File(...)):
         image = Image.open(io.BytesIO(contents)).convert("RGB")
         image_tensor = transform(image).unsqueeze(0).to(device)
         
-        with torch.no_grad():
-            output = model(image_tensor)
-            probabilities = torch.softmax(output, dim=1)
-            predicted_idx = torch.argmax(probabilities, dim=1).item()
-            predicted_class = class_names[predicted_idx]
-            probability = float(probabilities[0][predicted_idx])
-            all_classes = {class_names[i]: float(probabilities[0][i]) for i in range(len(class_names))}
+        predicted_idx, probabilities = get_prediction(image_tensor)
         
-        # возвращает JSON
+        predicted_class = class_names[predicted_idx]
+        probability = float(probabilities[predicted_idx])
+        all_classes = {class_names[i]: float(probabilities[i]) for i in range(len(class_names))}
+        
         return {
             "class": predicted_class,
             "probability": probability,
@@ -56,7 +60,7 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка {str(e)}")
 
-# реализуйте запуск приложения через Uvicorn.
+# Реализуйте запуск приложения через Uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
